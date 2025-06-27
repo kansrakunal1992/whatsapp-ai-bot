5676eb16caf0e8417e2de78dbf6d123a0ba5755d
kansrakunal1992 <kansrakunal@gmail.com>
2025-06-27 12:20:34 +0530

    Create _middleware.js

---
+++ api/_middleware.js
@@ -0,0 +1,7 @@
+export default function middleware(req) {
+  const res = new Response();
+  res.headers.set('Access-Control-Allow-Origin', '*');
+  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  return res;
+}
