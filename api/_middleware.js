1c8c92709470818570b8332fbac8ae3664585580
kansrakunal1992 <kansrakunal@gmail.com>
2025-06-30 16:20:28 +0530

    Update _middleware.js

--- api/_middleware.js
+++ api/_middleware.js
@@ -1,7 +1,17 @@
 export default function middleware(req) {
-  const res = new Response();
-  res.headers.set('Access-Control-Allow-Origin', '*');
-  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  // Set CORS headers
+  const res = new Response(JSON.stringify({ message: "Success" }), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    }
+  });
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return new Response(null, { status: 204 });
+  }
+
   return res;
 }
